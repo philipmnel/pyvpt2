b67ac5eac4cb7076475f0800081d29b1f011d9ee
philipmnel <philip.nelson97@gmail.com>
2021-11-15 11:30:18 -0500

    added hessian and gradient tests

--- pyvpt2/tests/test_pyvpt2.py
+++ pyvpt2/tests/test_pyvpt2.py
@@ -18,6 +18,40 @@ def test_pyvpt2_imported():
 
 def test_vpt2_energy():
     
+    mol = psi4.geometry("""
+    nocom
+    noreorient
+
+    O
+    H 1 R1
+    H 1 R2 2 A
+   
+    R1    =        0.94731025924472878064
+    R2    =        0.94731025924472878064
+    A    =      105.5028950965639
+    """)
+
+    psi4.set_options({'d_convergence': 1e-12,
+                'e_convergence': 1e-12,
+                'scf_type': 'direct',
+                'puream': True})
+
+    options = {'METHOD': 'scf/6-31g*',
+            'FD': 'ENERGY',
+            'DISP_SIZE': 0.050}
+
+    cfour_omega = [1826.8154, 4060.2203, 4177.8273]
+    cfour_anharmonic = [-54.0635, -158.2345, -177.9707] 
+
+    omega, anharmonic = pyvpt2.vpt2(mol, options)
+    omega = omega[-3:]
+    anharmonic = anharmonic[-3:]
+
+    assert psi4.compare_values(cfour_omega, omega, 0.1)
+    assert psi4.compare_values(cfour_anharmonic, anharmonic, 0.1)
+
+def test_vpt2_gradient():
+    
     psi4.set_memory('32gb')
     psi4.core.set_num_threads(6)
 
@@ -41,7 +75,45 @@ def test_vpt2_energy():
 
     
     options = {'METHOD': 'scf/6-31g*',
-            'FD': 'ENERGY',
+            'FD': 'GRADIENT',
+            'DISP_SIZE': 0.050}
+
+    cfour_omega = [1826.8154, 4060.2203, 4177.8273]
+    cfour_anharmonic = [-54.0635, -158.2345, -177.9707] 
+
+    omega, anharmonic = pyvpt2.vpt2(mol, options)
+    omega = omega[-3:]
+    anharmonic = anharmonic[-3:]
+
+    assert psi4.compare_values(cfour_omega, omega, 0.1)
+    assert psi4.compare_values(cfour_anharmonic, anharmonic, 0.1)
+    
+def test_vpt2_hessian():
+    
+    psi4.set_memory('32gb')
+    psi4.core.set_num_threads(6)
+
+    mol = psi4.geometry("""
+    nocom
+    noreorient
+
+    O
+    H 1 R1
+    H 1 R2 2 A
+   
+    R1    =        0.94731025924472878064
+    R2    =        0.94731025924472878064
+    A    =      105.5028950965639
+    """)
+
+    psi4.set_options({'d_convergence': 1e-12,
+                'e_convergence': 1e-12,
+                'scf_type': 'direct',
+                'puream': True})
+
+    
+    options = {'METHOD': 'scf/6-31g*',
+            'FD': 'HESSIAN',
             'DISP_SIZE': 0.050}
 
     cfour_omega = [1826.8154, 4060.2203, 4177.8273]
