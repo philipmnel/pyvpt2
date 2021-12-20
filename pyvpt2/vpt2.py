087fc554a17f9c39ac449728f564fd6d00d7d4f1
philipmnel <philip.nelson97@gmail.com>
2021-12-20 16:32:02 -0500

    added fermi resonance check; added bands + overtones

--- pyvpt2/vpt2.py
+++ pyvpt2/vpt2.py
@@ -2,8 +2,9 @@ import psi4
 import numpy as np
 import qcelemental as qcel
 import itertools
+import math
 
-import quartic
+from . import quartic
 
 def harmonic(mol, options):
     """
@@ -17,7 +18,7 @@ def harmonic(mol, options):
 
     method = options["METHOD"]
 
-    E0, wfn = psi4.frequency(method, molecule=mol, return_wfn=True)
+    E0, wfn = psi4.frequency(method, dertype=1, molecule=mol, return_wfn=True)
 
     omega = wfn.frequency_analysis["omega"].data
     modes = wfn.frequency_analysis["x"].data
@@ -154,6 +155,27 @@ def vpt2(mol, options=None):
         if abs(zeta[i, j, k]) > 1e-5:
             print(i + 1, j + 1, k + 1, "    ", zeta[i, j, k])
 
+    # Identify Fermi resonances:
+
+    fermi1 = np.zeros((n_modes, n_modes), dtype=bool)
+    fermi2 = np.zeros((n_modes, n_modes, n_modes), dtype=bool)
+    delta_omega_threshold = 100
+    delta_K_threshold = 10
+
+
+    for [i, j] in itertools.combinations(v_ind, 2):
+        if abs(2 * omega[i] - omega[j]) <=  delta_omega_threshold:
+            if phi_ijk[i,i,j]**4 / (256*(2*omega[i] - omega[j])**3) <= delta_K_threshold:
+                fermi1[i,j] = True
+                fermi1[j,i] = True
+
+    for [i, j, k] in itertools.combinations(v_ind,3):
+        if abs(omega[i] + omega[j] - omega[k]) <= delta_omega_threshold:
+            if phi_ijk[i,j,k]**4 / (64* (omega[i] + omega[j] - omega[k])**3) <= delta_K_threshold:
+                for [ii,jj,kk] in itertools.permutations([i,j,k]):
+                    fermi2[ii,jj,kk] = True
+
+
     chi = np.zeros((n_modes, n_modes))
     chi0 = 0.0
 
@@ -170,7 +192,11 @@ def vpt2(mol, options=None):
 
                 for k in v_ind:
 
-                    chi[i, i] -= ((8 * omega[i] ** 2 - 3 * omega[k] ** 2) * phi_ijk[i, i, k] ** 2) / (omega[k] * (4 * omega[i] ** 2 - omega[k] ** 2))
+                    if fermi1[i,k]:
+                        chi[i,i] -= (phi_ijk[i, i, k] ** 2 ) / 2 * (1 / (2 * omega[i] + omega[k]) + 4 / omega[k])
+
+                    else:
+                        chi[i, i] -= ((8 * omega[i] ** 2 - 3 * omega[k] ** 2) * phi_ijk[i, i, k] ** 2) / (omega[k] * (4 * omega[i] ** 2 - omega[k] ** 2))
 
                 chi[i, i] /= 16
 
@@ -190,12 +216,20 @@ def vpt2(mol, options=None):
 
                     chi[i, j] -= (phi_ijk[i, i, k] * phi_ijk[j, j, k]) / omega[k]
 
-                    delta = omega[i] + omega[j] - omega[k]
-                    delta *= omega[i] + omega[j] + omega[k]
-                    delta *= omega[i] - omega[j] + omega[k]
-                    delta *= omega[i] - omega[j] - omega[k]
+                    if fermi2[i, j, k]:
+                        temp = 1 / (omega[i] + omega[j] + omega[k])
+                        temp += 1 / (-omega[i] + omega[j] + omega[k])
+                        temp += 1 / (omega[i] - omega[j] + omega[k])
 
-                    chi[i, j] += 2 * omega[k] * (omega[i] ** 2 + omega[j] ** 2 - omega[k] ** 2) * phi_ijk[i, j, k] ** 2 / delta
+                        chi[i, j] += (phi_ijk[i, j, k] ** 2) * temp / 2
+
+                    else:
+                        delta = omega[i] + omega[j] - omega[k]
+                        delta *= omega[i] + omega[j] + omega[k]
+                        delta *= omega[i] - omega[j] + omega[k]
+                        delta *= omega[i] - omega[j] - omega[k]
+
+                        chi[i, j] += 2 * omega[k] * (omega[i] ** 2 + omega[j] ** 2 - omega[k] ** 2) * phi_ijk[i, j, k] ** 2 / delta
 
                     if (j > i) and (k > j):
                         chi0 -=  16 * (omega[i] * omega[j] * omega[k] * phi_ijk[i, j, k] ** 2) / delta
@@ -217,18 +251,38 @@ def vpt2(mol, options=None):
             print(i + 1, j + 1, "    ", chi[i, j])
 
     anharmonic = np.zeros(n_modes)
+    overtone = np.zeros(n_modes)
+    band = np.zeros((math.comb(len(v_ind), 2), 3))
 
     for i in v_ind:
-
         anharmonic[i] = 2 * chi[i, i]
+        overtone[i] = 6 * chi[i,i]
 
         for j in v_ind:
-            if j != i:
-                anharmonic[i] += 0.5 * chi[i, j]
+            if j == i: continue
+            anharmonic[i] += 0.5 * chi[i, j]
+            overtone[i] += chi[i, j]
 
-    print("\n FREQ (cm-1):")
+    for indx, [i, j] in enumerate(itertools.combinations(v_ind, 2)):
+        band[indx, 1] = i; band[indx, 2] = j  
+        band[indx, 0] = 2 * chi[i, i] + 2 * chi[j, j] + 2 * chi[i, j]
+        for k in v_ind:
+            if k == i: continue
+            elif k == j: continue
+            band[indx, 0] += 0.5 * (chi[i,k] + chi[j,k])
+
+    print("\n Fundamentals FREQ (cm-1):")
     for i in v_ind:
         print(i + 1, omega[i], anharmonic[i], (omega[i] + anharmonic[i]), sep="    ")
+
+    print("\n Overtones FREQ (cm-1):")
+    for i in v_ind:
+        print("2 " + str(i+1), 2*omega[i], overtone[i], (2*omega[i] + overtone[i]), sep="   ") 
+
+    print("\n Combination Bands FREQ (cm-1:")
+    for [band_ij, i, j] in band:
+        i = int(i); j = int(j)
+        print(i+1, j+1, (omega[i] + omega[j]), band_ij, (omega[i] + omega[j] + band_ij), sep="    ")
 
     print("\n ZPE:")
     print(zpe)
