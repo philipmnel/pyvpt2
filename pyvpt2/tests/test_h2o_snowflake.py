d2eec274affe30328418f8e6bd13d83b891b6a65
philipmnel <philip.nelson97@gmail.com>
2023-05-17 17:04:39 -0400

    hessians only

--- pyvpt2/tests/test_h2o_snowflake.py
+++ pyvpt2/tests/test_h2o_snowflake.py
@@ -12,7 +12,7 @@ except:
     no_qcfractal = True
 
 @pytest.mark.skipif(no_qcfractal, reason="QCFractal not installed")
-@pytest.mark.parametrize("driver", ["ENERGY", "GRADIENT", "HESSIAN"])
+@pytest.mark.parametrize("driver", ["HESSIAN"])
 def test_h2o_snowflake_vpt2(driver):
 
     snowflake = FractalSnowflake()
