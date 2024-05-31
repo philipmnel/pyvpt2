5c259c9c0f45e516b8159cd828a1393ec81bb22b
Philip Nelson <71409323+philipmnel@users.noreply.github.com>
2024-05-31 13:33:07 -0400

    Add runtime version attribute (#48)

--- pyvpt2/__init__.py
+++ pyvpt2/__init__.py
@@ -1,8 +1,14 @@
 """Package to calculate VPT2 anharmonic corrections."""
 
 import logging
+from importlib.metadata import PackageNotFoundError, version
 
 from .vpt2 import *
+
+try:
+    __version__ = version("pyvpt2")
+except PackageNotFoundError:
+    pass
 
 vpt2_log = logging.getLogger("psi4.pyvpt2")
 vpt2_log.propagate = True
